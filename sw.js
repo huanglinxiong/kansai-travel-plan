393a84d0d161676d1ea4a9e7ebbd6fc9f5398e1c
nobita <1499942825@qq.com>
2026-05-25 20:49:28 +0800

    init: 关西旅行计划 PWA

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'kansai-trip-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './data/itinerary.json',
+    './manifest.json',
+    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(cached => {
+            const fetched = fetch(event.request).then(response => {
+                if (response && response.status === 200) {
+                    const clone = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+                }
+                return response;
+            }).catch(() => cached);
+            return cached || fetched;
+        })
+    );
+});
